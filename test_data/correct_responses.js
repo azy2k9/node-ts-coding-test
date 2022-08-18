b97ee789d148fb72424aea7735af888b746ce8be
evergreen-arslaan-qadus <74248128+evergreen-arslaan-qadus@users.noreply.github.com>
2022-08-18 12:22:54 +0100

    Initial project commit

---
+++ test_data/correct_responses.js
@@ -0,0 +1,77 @@
+module.exports.mock_outages_to_upload = [
+  {
+    begin: "2022-01-12T08:11:21.333Z",
+    end: "2022-12-13T07:20:57.984Z",
+    id: "correct_id",
+    name: "battery 1",
+  },
+  {
+    begin: "2022-03-12T08:11:21.333Z",
+    end: "2022-12-13T07:20:57.984Z",
+    id: "correct_id",
+    name: "battery 1",
+  },
+  {
+    begin: "2022-01-01T00:00:00.000Z",
+    end: "2022-12-13T07:20:57.984Z",
+    id: "correct_id",
+    name: "battery 1",
+  },
+];
+
+module.exports.outages_to_upload = [
+  {
+    begin: "2022-02-15T11:28:26.735Z",
+    end: "2022-08-28T03:37:48.568Z",
+    id: "0e4d59ba-43c7-4451-a8ac-ca628bcde417",
+    name: "Battery 6",
+  },
+  {
+    begin: "2022-01-01T00:00:00.000Z",
+    end: "2022-09-15T19:45:10.341Z",
+    id: "111183e7-fb90-436b-9951-63392b36bdd2",
+    name: "Battery 1",
+  },
+  {
+    begin: "2022-02-18T01:01:20.142Z",
+    end: "2022-08-15T14:34:50.366Z",
+    id: "111183e7-fb90-436b-9951-63392b36bdd2",
+    name: "Battery 1",
+  },
+  {
+    begin: "2022-02-15T11:28:26.965Z",
+    end: "2023-12-24T14:20:37.532Z",
+    id: "20f6e664-f00e-4621-9ca4-5ec588aadeaf",
+    name: "Battery 7",
+  },
+  {
+    begin: "2022-04-08T16:29:22.128Z",
+    end: "2022-06-09T22:10:59.718Z",
+    id: "70656668-571e-49fa-be2e-099c67d136ab",
+    name: "Battery 3",
+  },
+  {
+    begin: "2023-05-11T14:35:15.359Z",
+    end: "2023-12-27T11:19:19.393Z",
+    id: "75e96db4-bba2-4035-8f43-df2cbd3da859",
+    name: "Battery 8",
+  },
+  {
+    begin: "2022-02-16T07:01:50.149Z",
+    end: "2022-10-03T07:46:31.410Z",
+    id: "86b5c819-6a6c-4978-8c51-a2d810bb9318",
+    name: "Battery 2",
+  },
+  {
+    begin: "2022-05-09T04:47:25.211Z",
+    end: "2022-12-02T18:37:16.039Z",
+    id: "86b5c819-6a6c-4978-8c51-a2d810bb9318",
+    name: "Battery 2",
+  },
+  {
+    begin: "2022-02-23T11:33:58.552Z",
+    end: "2022-12-16T00:52:16.126Z",
+    id: "a79fe094-087b-4b1e-ae20-ac4bf7fa429b",
+    name: "Battery 5",
+  },
+];
